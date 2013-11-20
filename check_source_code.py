3cc9e94f027a90fc5d917d1658849d4ee844498a
Jian Yuan <jianyuan@gmail.com>
2013-11-20 14:11:13 +0000

    remove check

--- check_source_code.py
+++ check_source_code.py
@@ -70,7 +70,8 @@ def check_source_code(source_code):
     #     res = aeval(source_code)
 
         wrapper_path = os.path.join(os.path.dirname(__file__), 'eval_wrapper.py')
-        sp = Popen(['python', wrapper_path, '-c', source_code], stdout=PIPE)
+        # sp = Popen(['python', wrapper_path, '-c', source_code], stdout=PIPE)
+        sp = Popen(['python', '-c', source_code], stdout=PIPE)
         res = sp.communicate()[0]
 
         # if 'decide' not in aeval.symtable or not isinstance(aeval.symtable['decide'], asteval.asteval.Procedure):
---
+++ tourney/evolution.py
@@ -0,0 +1,48 @@
+# http://www.lifl.fr/IPD/ipd.html.en
+
+from Player import Player
+from Gamemaster import Gamemaster
+import random
+
+def evolution(iterations=None, players=[]):
+    # Must have 2 or more players to play round robin!
+    if len(players) >= 2:
+        if iterations is None:
+            iterations = random.randint(100, 150)
+
+        gm = Gamemaster(iterations=iterations)
+
+        for player in players:
+            gm.add_player(*player)
+
+        gm.generate_matches()
+
+        gm.start_tournament()
+
+        # d = gm.get_overall_points()
+        # for v in sorted(d, key=d.get, reverse=False):
+        #     print v, d[v]
+
+        output = {
+            'points': gm.get_overall_points(),
+            'winner': gm.get_winner(),
+            'results': gm.get_match_results()
+        }
+
+    else:
+        output = {
+            'points': {},
+            'winner': None,
+            'results': []
+        }
+
+    return output
+
+if __name__ == '__main__':
+
+    players = [
+        [1, 'def decide(context): return context.iterationstest'],
+        [2, 'def decide(context): return "C"']
+    ]
+
+    print round_robin(iterations=random.randint(100, 150), players=players)
